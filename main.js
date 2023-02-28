1841dfed623706d138c78a9d12b2e816e77f4db2
randomanalysis <115797746+randomanalysis@users.noreply.github.com>
2023-02-28 16:32:39 +0000

    styled book card, fixed showLibrary bug

--- main.js
+++ main.js
@@ -1,31 +1,56 @@
 const myLibrary = [];
-const booksDisplay = document.getElementById("book-display")
+const booksDisplay = document.getElementById("book-display");
+const bookFormPopUp = document.getElementById("bookFormPopUp");
+const bookForm = document.getElementById("bookForm");
+
+
+window.onload = displayLibrary
 
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary(aBook) {
-    if (!myLibrary.includes(aBook)) {
+    if (!checkIfTitleInLibrary(aBook)) {
         myLibrary.push(aBook);
     }
 }
 
+function checkIfTitleInLibrary(aBook) {
+    for (const book of myLibrary) {
+        if (book.title == aBook.title && book.author == aBook.author) {
+            alert(`The book '${book.title}' aleady exists in your library.`);
+            return true;
+            
+        }
+    }
+    return false
+}
+
 function displayLibrary() {
 
-    const newBook = new Book("Test Title", "Test Author", 111, true);
-    const otherBook = new Book("Test Title 2", "Test Author 2", 222, false);
+    //const newBook = new Book("Test Title", "Test Author", 111, true);
+    //const otherBook = new Book("Test Title 2", "Test Author 2", 222, false);
 
-    addBookToLibrary(newBook);
-    addBookToLibrary(otherBook);
+    //addBookToLibrary(newBook);
+    //addBookToLibrary(otherBook);
+
+    removeElementsByClass("bookCard")
 
     for (const book of myLibrary) {
         displayBookAsCard(book);
     }
 
+}
+
+function removeElementsByClass(className){
+    const elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
 }
 
 function displayBookAsCard(aBook) {
@@ -46,7 +71,7 @@ function displayBookAsCard(aBook) {
     bookTitle.textContent += aBook.title;
     bookAuthor.textContent += aBook.author;
     bookPages.textContent += `${aBook.pages} pages`;
-    if (aBook.read = true) {
+    if (aBook.read == true) {
         bookRead.textContent += "Read";
         bookRead.classList.add("book-read");
     } else {
@@ -65,12 +90,29 @@ function displayBookAsCard(aBook) {
 
 }
 
-window.onload = displayLibrary
-
 function openForm() {
-  document.getElementById("bookForm").style.display = "block";
+    bookFormPopUp.style.display = "block";
 }
 
 function closeForm() {
-  document.getElementById("bookForm").style.display = "none";
+    bookFormPopUp.style.display = "none";
+}
+
+const getBookFromInput = () => {
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const read = document.getElementById('read').checked
+    return new Book(title, author, pages, read)
+  }
+
+
+function bookFormSubmit() {
+    const newBook = getBookFromInput()
+    addBookToLibrary(newBook);
+    ///alert(`New Book '${newBook.title}' added to your library.`);
+    displayLibrary();
+    bookForm.reset();
+    closeForm();
+
 }
